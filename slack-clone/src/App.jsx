0938498b7145162a6cd0230713a94e23f9a2fc1f
vinod120 <vinodgate2016@gmail.com>
2020-12-20 19:14:22 +0530

    app-created

---
+++ slack-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Slack-clone</h1>
+      {/* Header */}
+      {/* app__body */}
+      {/* Sidebar */}
+      {/* React-router chat screen */}
+    </div>
+  );
+}
+
+export default App;
