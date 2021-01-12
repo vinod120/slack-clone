e1f67ed80b51ec5f111ada436849f54cab387482
vinod120 <vinodgate2016@gmail.com>
2021-01-12 17:24:54 +0530

    project setup completed

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
-      {/* <h1>Slack clone</h1> */}
-      <Header />
-      {/* Sidebar */}
+      <h1>Slack clone</h1>
+      {/* <Header /> */}
+      {/* <Sidebar /> */}
       {/* Routing chat-app */}
     </div>
   );
