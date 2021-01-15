3289e51bf73eca545843df77847887127f93fef7
vinod120 <vinodgate2016@gmail.com>
2021-01-15 09:59:35 +0530

    add channel function added

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import Header from './components/Header';
 import { Sidebar } from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Chat from './components/Chat';
 function App() {
   return (
     <div className="App">
       <h1>Slack clone</h1>
-      {/* <Header /> */}
-      <Header />
-      <div className="app_body">
-        {/* <Sidebar /> */}
-        <Sidebar />
-        {/* Routing chat-app */}
-      </div>
+      <Router>
+        {/* <Header /> */}
+        <Header />
+        <div className="app_body">
+          {/* <Sidebar /> */}
+          <Sidebar />
+          {/* Routing chat-app screen */}
+          <Switch>
+            <Route path="/rooms/:roomId">
+              {/* <h1>Chat Screen</h1> */}
+              {/* Chat */}
+              <Chat />
+            </Route>
+            <Route path='/'>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ slack-clone/src/components/Chat.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import './Chat.css';
+
+const Chat = () => {
+    const {roomId} = useParams();
+    return (
+        <>
+            <div className="chat">
+                {/* <h2>You are in the {roomId} room</h2> */}
+                <div className="chat_header">
+                    <div className="chat_headerLeft">
+                        <h4 className="chat_channelName">
+                            <strong>#general</strong>
+                            <StarBorderOutlinedIcon />
+                        </h4>
+                    </div>
+                    <div className="chat_headerRight">
+                        <p>
+                            <InfoOutlinedIcon /> Details
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Chat
